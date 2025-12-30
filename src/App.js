651b6932674cc0db3b581d99de79e98c7a5af51b
Karim111171 <karim.keirouz@gmail.com>
2025-12-31 00:12:22 +0200

    modif css et nav

--- src/App.js
+++ src/App.js
@@ -10,22 +10,29 @@ const App = () => {
     <Router> {/* Wrap the entire app with Router */}
       <div className="App">
         {/* Header */}
-        <header>
-          <nav>
-	    <h1>Carla Dib Keirouz</h1>
-            <ul>
-		  <li><a 
-          href="https://karim111171.github.io/Carla-Dib-Keirouz/"  // remplace par l’URL finale de ton portfolio
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          About
-        </a></li>
-              <li><Link to="/about">Home</Link></li>
-              <li><Link to="/products">Collect</Link></li>
-              <li><Link to="/contact">Contact me</Link></li>
-            </ul>
-          </nav>
+	  <header className="header">
+          <div className="container header-content">
+            <h1>Carla Dib Keirouz</h1>
+            <nav className="nav">
+              <ul>
+                <li>
+                  <Link to="/products">Paintings</Link>
+                </li>
+                <li>
+                  <Link to="/success">Checkout</Link>
+                </li>
+                <li>
+                  <a
+                    href="https://ton-portfolio.com" // Remplace par l'URL de ton portfolio
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Portfolio
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </header>
 
         {/* Main Content */}
